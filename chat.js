7c576b810bdb7fe2974553226944b26c3b0c6317
GustavoA-maker <84721352+GustavoA-maker@users.noreply.github.com>
2024-10-24 08:25:34 -0300

    Delete chat.js

--- chat.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('denuncia-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    
- 
-    
-    alert('Denúncia enviada com sucesso!'); 
-    document.getElementById('denuncia-form').reset(); 
-});
